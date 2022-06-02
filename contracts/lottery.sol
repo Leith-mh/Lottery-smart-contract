efd3dcb7e744e9a6d86fb61fdca4235d4224790a
"Leith" <"leith.mhf@gmail.com">
2022-06-02 02:12:48 +0100

    random number generating function

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -11,10 +11,16 @@ contract Lottery{
     }
 
     receive() external payable{
+        require(msg.value == 0.1 ether);
         players.push(payable(msg.sender));
     }
 
     function getBalance() public view returns(uint){
+        require(msg.sender == manager);
         return address(this).balance;
+    }
+
+    function random() public view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,players.length)));
     }
 }
